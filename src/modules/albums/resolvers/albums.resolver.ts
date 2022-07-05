a7eb1c38e698106c6d3a524e73d63d054a2f11fb
Serhii1108 <68705512+Serhii1108@users.noreply.github.com>
2022-07-05 20:52:21 +0300

    feat: implement resolve fields in tracks

--- src/modules/albums/resolvers/albums.resolver.ts
+++ src/modules/albums/resolvers/albums.resolver.ts
@@ -7,7 +7,14 @@ import {
   Resolver,
 } from "@nestjs/graphql";
 
-import { Album, AlbumInput, DeleteResponse, Genre } from "src/types/graphql.js";
+import {
+  Album,
+  AlbumInput,
+  Artist,
+  Band,
+  DeleteResponse,
+  Genre,
+} from "src/types/graphql.js";
 import { albumsService } from "../services/albums.service.js";
 import { BandsService } from "../../bands/services/bands.service.js";
 import { ArtistsService } from "../../artists/services/artists.service.js";
@@ -67,14 +74,14 @@ export class AlbumsResolver {
   @ResolveField()
   async bands(@Parent() album: { bandsIds: string[] }) {
     const ids: string[] = album.bandsIds;
-    const genres: Genre[] = await this.bandsService.findByIds(ids);
-    return genres;
+    const bands: Band[] = await this.bandsService.findByIds(ids);
+    return bands;
   }
 
   @ResolveField()
   async artists(@Parent() album: { artistsIds: string[] }) {
     const ids: string[] = album.artistsIds;
-    const artists: Album[] = await this.artistsService.findByIds(ids);
+    const artists: Artist[] = await this.artistsService.findByIds(ids);
     return artists;
   }
 
--- src/modules/tracks/resolvers/tracks.resolver.ts
+++ src/modules/tracks/resolvers/tracks.resolver.ts
@@ -7,12 +7,31 @@ import {
   Resolver,
 } from "@nestjs/graphql";
 
-import { DeleteResponse, Track, TrackInput } from "src/types/graphql.js";
+import {
+  Album,
+  Artist,
+  Band,
+  DeleteResponse,
+  Genre,
+  Track,
+  TrackInput,
+} from "src/types/graphql.js";
+
 import { TracksService } from "../services/tracks.service.js";
+import { ArtistsService } from "../../artists/services/artists.service.js";
+import { BandsService } from "../../bands/services/bands.service.js";
+import { GenresService } from "../../genres/services/genres.service.js";
+import { albumsService } from "../../albums/services/albums.service.js";
 
 @Resolver("Track")
 export class TracksResolver {
-  constructor(private readonly tracksService: TracksService) {}
+  constructor(
+    private readonly tracksService: TracksService,
+    private readonly albumsService: albumsService,
+    private readonly bandsService: BandsService,
+    private readonly artistsService: ArtistsService,
+    private readonly genresService: GenresService
+  ) {}
 
   @Query()
   async track(@Args("id") id: string): Promise<Track> {
@@ -54,5 +73,33 @@ export class TracksResolver {
   @ResolveField()
   async id(@Parent() track: { _id: string }) {
     return track._id;
+  }
+
+  @ResolveField()
+  async bands(@Parent() track: { bandsIds: string[] }) {
+    const ids: string[] = track.bandsIds;
+    const bands: Band[] = await this.bandsService.findByIds(ids);
+    return bands;
+  }
+
+  @ResolveField()
+  async artists(@Parent() track: { artistsIds: string[] }) {
+    const ids: string[] = track.artistsIds;
+    const artists: Artist[] = await this.artistsService.findByIds(ids);
+    return artists;
+  }
+
+  @ResolveField()
+  async genres(@Parent() track: { genresIds: string[] }) {
+    const ids: string[] = track.genresIds;
+    const genres: Genre[] = await this.genresService.findByIds(ids);
+    return genres;
+  }
+
+  @ResolveField()
+  async album(@Parent() track: { albumId: string }) {
+    const id = track.albumId;
+    const album: Album = await this.albumsService.findById(id);
+    return album;
   }
 }
